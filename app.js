188d13b89f29f2031f3621a3fb8eb143e7118451
checkiamsiam <issiam02415@gmail.com>
2022-02-13 20:30:37 +0600

    completed

---
+++ app.js
@@ -0,0 +1,34 @@
+document.getElementById('generate-btn').addEventListener('click', function () {
+  var letter1 = String(Math.floor(Math.random() * 10));
+  var letter2 = String(Math.floor(Math.random() * 10));
+  var letter3 = String(Math.floor(Math.random() * 10));
+  var letter4 = String(Math.floor(Math.random() * 10));
+  document.getElementById('generate-field').value = letter1 + letter2 + letter3 + letter4;
+})
+var calcBtn = document.getElementsByClassName('button');
+for (const btn of calcBtn) {
+  btn.addEventListener('click', function (e) {
+    document.getElementById('confirm-field').value += e.target.innerText;
+    if (e.target.innerText == 'C') {
+      document.getElementById('confirm-field').value = '';
+    }
+    if (e.target.innerText == '<') {
+     var confirm = document.getElementById('confirm-field');
+       var  d = confirm.value.substring(0, confirm.value.length-2);
+        document.getElementById('confirm-field').value = d;
+    }
+  })
+}
+document.getElementById('submit-btn').addEventListener('click', function () {
+  if (document.getElementById('generate-field').value == document.getElementById('confirm-field').value) {
+    document.getElementById('right').style.display = 'block';
+    document.getElementById('wrong').style.display = 'none';
+  } else {
+    document.getElementById('wrong').style.display = 'block';
+    document.getElementById('right').style.display = 'none';
+    document.getElementById('trail-left').innerText = Number(document.getElementById('trail-left').innerText) - 1;
+    if (Number(document.getElementById('trail-left').innerText) < 0) {
+      document.getElementById('trail').innerText = 'generate pin again'
+    }
+  }
+})
